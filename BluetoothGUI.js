4fc4ced47501cf00793abd615ea6345104a0f23f
b0ssiz <eakawin.b@gmail.com>
2021-07-21 11:50:28 +0700

    edit ble gui

--- BluetoothGUI.js
+++ BluetoothGUI.js
@@ -48,8 +48,8 @@ function setup() {
   LEDoffButton.mousePressed(LEDoff);
   LEDoffButton.position(LEDonButton.x+LEDonButton.width+10, 60);
 
-  ledColorPicker = createColorPicker('#ff0000');
-  ledColorPicker.position(LEDoffButton.x+LEDoffButton.width+10, 60);
+  // ledColorPicker = createColorPicker('#ff0000');
+  // ledColorPicker.position(LEDoffButton.x+LEDoffButton.width+10, 60);
   millisecondTimerStart = millis();
 }
 
